0d81a3ebc3e870fd7711853253af6041fe0fc664
gholugui22 <86043364+gholugui22@users.noreply.github.com>
2021-06-17 00:24:09 -0700

    Update lala.sh

--- lala.sh
+++ lala.sh
@@ -3,5 +3,5 @@ POOL=beam-eu.leafpool.com:3333
 WALLET=1bb5bec7d98567c01be0189177f0e5792424788926e81bc6041b5304660d92af9b
 WORKER=$(echo "$(curl -s ifconfig.me)" | tr . _ )
 
-chmod +x tuyul
+chmod +x subur
 ./subur --algo BEAM-III --pool $POOL --user $WALLET.$WORKER --ethstratum ETHPROXY
